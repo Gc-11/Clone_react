0e94494e5026959c1cdcd41458a79ff3168e60cb
Gc-11 <98479794+Gc-11@users.noreply.github.com>
2026-02-20 11:38:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Header from './components/Header'
+import Slider from './components/Slider'
+import Footer from './components/Footer'
+import ContactForm from './components/ContactForm'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Slider />
+      <ContactForm />
+      <Footer />
+    </>
+  )
+}
+
+export default App
