c519b1d7a11bfcf938860b0647dcb27e5ecdfd2c
Gc-11 <98479794+Gc-11@users.noreply.github.com>
2026-02-27 11:18:54 +0530

    Refactor Header component to accept props
    Refactor Header component to accept toggleTheme and darkMode props. Update button for theme toggle and improve layout.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,31 @@
-
 import { useState } from "react"
 import FullscreenNav from "./FullscreenNav"
 
-function Header() {
+function Header({ toggleTheme, darkMode }) {
   const [open, setOpen] = useState(false)
 
   return (
     <>
       <header>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/b/bf/KFC_logo.svg" alt="KFC" />
-        <div className="menu" onClick={() => setOpen(true)}>☰</div>
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/b/bf/KFC_logo.svg" 
+          alt="KFC" 
+        />
+
+        <div style={{ display: "flex", gap: "15px", alignItems: "center" }}>
+          
+        
+          <button 
+            onClick={toggleTheme} 
+            className="theme-btn"
+          >
+            {darkMode ? "☀️" : "🌙"}
+          </button>
+
+          <div className="menu" onClick={() => setOpen(true)}>☰</div>
+        </div>
       </header>
+
       <FullscreenNav open={open} setOpen={setOpen} />
     </>
   )
