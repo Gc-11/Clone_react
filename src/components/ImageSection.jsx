e0f21babd48a965918f21b35e2757fd232d20ce4
Gc-11 <98479794+Gc-11@users.noreply.github.com>
2026-02-27 11:30:45 +0530

    Update ImageSection.jsx

--- src/components/ImageSection.jsx
+++ src/components/ImageSection.jsx
@@ -1,15 +1,25 @@
-
-function ImageSection({ title, text, image }) {
+function ImageSection({ title, text, image, link }) {
   return (
     <section 
       className="image-section"
       style={{ backgroundImage: `url(${image})` }}
     >
       <div className="overlay"></div>
+
       <div className="section-content">
         <h2>{title}</h2>
         <p>{text}</p>
-        <button className="btn">Learn More</button>
+
+        <a 
+          href={link} 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          <button className="btn">
+            Learn More
+          </button>
+        </a>
+
       </div>
     </section>
   )
